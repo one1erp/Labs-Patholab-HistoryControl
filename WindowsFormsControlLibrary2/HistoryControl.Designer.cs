88b3b569145203a232c613796d170b6b69bf6398
one1erp <avigaile@one1.co.il>
2024-04-08 17:25:05 +0300

    Initial commit with additional files

---
+++ WindowsFormsControlLibrary2/HistoryControl.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsControlLibrary2
+{
+    partial class HistoryControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbMedical = new System.Windows.Forms.ListBox();
+            this.radListControl1 = new Telerik.WinControls.UI.RadListControl();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radListControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lbMedical, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.radListControl1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 119F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(258, 497);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbMedical
+            // 
+            this.lbMedical.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbMedical.FormattingEnabled = true;
+            this.lbMedical.Location = new System.Drawing.Point(3, 3);
+            this.lbMedical.Name = "lbMedical";
+            this.lbMedical.Size = new System.Drawing.Size(252, 113);
+            this.lbMedical.TabIndex = 0;
+            // 
+            // radListControl1
+            // 
+            this.radListControl1.AutoSizeItems = true;
+            this.radListControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radListControl1.Location = new System.Drawing.Point(3, 122);
+            this.radListControl1.Name = "radListControl1";
+            this.radListControl1.Size = new System.Drawing.Size(252, 372);
+            this.radListControl1.TabIndex = 1;
+            this.radListControl1.Text = "radListControl1";
+            // 
+            // HistoryControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "HistoryControl";
+            this.Size = new System.Drawing.Size(258, 497);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radListControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox lbMedical;
+        private Telerik.WinControls.UI.RadListControl radListControl1;
+
+    }
+}
---
+++ WindowsFormsControlLibrary2/HistoryControl.cs
@@ -0,0 +1,112 @@
+﻿using Patholab_DAL_V1;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace WindowsFormsControlLibrary2
+{
+    public partial class HistoryControl: UserControl
+    {
+        const string Medical_Title = "אין הסטוריה במדיקל";
+        public event Action<string> ItemSelected;
+        public SDG _sdg;
+        private DataLayer _dal;
+
+        public HistoryControl()
+        {
+            InitializeComponent();
+        }
+
+        public void ClearList()
+        {
+            lbMedical.Items.Clear();
+            radListControl1.Items.Clear();
+            //    lblVisit.Text = "";
+        
+        }
+
+        public void LoadData(string Cusdg, SDG sdg, DataLayer dal)
+        {
+            _dal = dal;
+            _sdg = sdg;
+            IQueryable<SDG_USER> Historylist = _dal.FindBy<SDG_USER>(x => x.U_PATIENT.Value == _sdg.SDG_USER.U_PATIENT.Value).Include(x => x.CLIENT).Include(a => a.SDG);
+
+            foreach (var item in Historylist)
+            {
+                if (item.SDG.NAME != Cusdg)
+                {
+
+                    RadListDataItem descriptionItem = new RadListDataItem();
+                    descriptionItem.Text = item.U_PATHOLAB_NUMBER + "     " + item.SDG.CREATED_ON.Value.ToString("dd/MM/yyyy");
+                    string imgN = string.Format("sdg{0}.ico", item.SDG.STATUS);
+                    descriptionItem.Image = new Bitmap(Path.Combine(Patholab_Common.Utils.GetResourcePath(), "sdg" + _sdg.STATUS + ".ico"));
+
+
+                    this.radListControl1.Items.Add(descriptionItem);
+
+                }
+            }
+
+            var client = Historylist.First().CLIENT.CLIENT_USER;// sdg.SDG_USER.CLIENT.CLIENT_USER;
+
+            List<string> split = new List<string>();
+            if (client.U_VISIT_1 != null)
+            {
+                split.AddRange(client.U_VISIT_1.Split(','));
+            }
+            if (client.U_VISIT_2 != null)
+            {
+                split.AddRange(client.U_VISIT_2.Split(','));
+
+            }
+
+            foreach (var row in split)
+            {
+                lbMedical.Items.Add(row);
+            }
+
+            //         label1.Text = string.Format ( "{0} \n {1}", client.U_VISIT_1, client.U_VISIT_2 );
+            //   }
+
+        }
+
+
+        private void radListControl1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                var sdg = (((Telerik.WinControls.UI.RadListControl)(sender)).SelectedItem).Text;
+
+                //Split sdg name from string
+                var INDX = sdg.IndexOf(' ');
+                var sdgName = sdg.Substring(0, INDX);
+                ItemSelected(sdgName);
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Cannot Load this sdg " + ex.Message, "Nautilus", MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        internal void LoadDat8a(string p, IQueryable<SDG_USER> list)
+        {
+            //   throw new NotImplementedException ( );
+        }
+
+        internal void LoadDa7ut8a(string p, IQueryable<SDG_USER> list)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
